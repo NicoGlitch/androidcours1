f39116cbf737c47bcbaf805af5033bad77a24acd
Nicolas Bégin <beg@hotmail.com>
2020-03-13 10:13:57 -0400

    exercice 1(groupe)

---
+++ app/src/main/java/com/nbegin/exercicecours1/GuessNumber.java
@@ -0,0 +1,32 @@
+package com.nbegin.exercicecours1;
+
+import java.util.Random;
+
+public class GuessNumber {
+    private int currentNumber;
+    private int maxNumber;
+    public enum guessState{BIGGER, EQUAL, SMALLER};
+
+    public GuessNumber(int maxNumber) {
+        this.maxNumber = maxNumber;
+        generateNewNumber();
+    }
+
+    public guessState testNumber(int numberToTest){
+        guessState numberState;
+
+        if(numberToTest > currentNumber){
+            numberState = guessState.SMALLER;
+        }else if (numberToTest == currentNumber){
+            numberState = guessState.EQUAL;
+        }else{
+            numberState= guessState.BIGGER;
+        }
+    return numberState;
+    }
+
+    public void generateNewNumber(){
+        currentNumber = new Random().nextInt( maxNumber+1);
+
+    }
+}
--- app/src/main/java/com/nbegin/exercicecours1/MainActivity.java
+++ app/src/main/java/com/nbegin/exercicecours1/MainActivity.java
@@ -3,12 +3,50 @@ package com.nbegin.exercicecours1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final int MAX = 10;
+    private GuessNumber guessnumber;
 
+    private EditText inputNumber;
+    private Button buttonTry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputNumber = findViewById(R.id.editText_nbr1);
+        buttonTry = findViewById(R.id.btn_compare);
+
+        guessnumber = new GuessNumber(MAX);
+        setListener();
+    }
+    private void setListener(){
+        buttonTry.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                guess();
+            }
+        });
+    }
+    private void guess(){
+        int guessNumber  = Integer.parseInt(inputNumber.getText().toString());
+        GuessNumber.guessState result =  guessnumber.testNumber(guessNumber);
+
+        if(result == GuessNumber.guessState.SMALLER){
+    showHint("Votre nombre est plus grand.");
+        }else if (result == GuessNumber.guessState.BIGGER){
+            showHint("Votre nombre est plus grand.");
+        }else{
+            showHint("Bravo !, un autre nombre à été généré");
+            guessnumber.generateNewNumber();
+        }
+    }
+    private void showHint(String hint){
+        Toast.makeText(this, hint, Toast.LENGTH_LONG).show();
     }
 }
